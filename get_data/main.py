0ee32d1f2a7845d1c82664e376e6a7db1f303a48
Aku1795 <mtvc95@gmail.com>
2021-11-21 16:14:22 +0100

    starting to package Extraction into classes

--- get_data/main.py
+++ get_data/main.py
@@ -3,6 +3,50 @@ import requests
 from bs4 import BeautifulSoup
 
 
+class Extractor
+
+
+
+class DJExtraction:
+
+    def __init__(self, url) -> None:
+        self._url = url
+    
+    def get_dj_label(self, dj_container):
+        try:
+            return dj_container.find("span",  {"class": "font-normal text-sm md:text-md lowercase"}).text
+        except:
+            return
+
+    def get_dj_name(self, dj_container):
+        dj_with_label = dj_container.find("span",  {"class": "font-bold"}).text
+        label = get_dj_label(dj_container)
+        if label:
+            dj_wo_label = dj_with_label.replace(label, "")
+            return (dj_wo_label, get_dj_label(dj_container))
+        return (dj_with_label, None)
+
+    def extract_djs(self, floor):
+        djs = floor.find_all("div", "running-order-set__info")
+        dj_names = [get_dj_name(dj) for dj in djs]
+
+        return dj_names 
+    
+    def extract(self):
+        soup = BeautifulSoup(crawl_url.content, "html.parser")
+    
+        first_floor = soup.find("div", {"data-set-floor":"1"})
+        first_floor = soup.find("div", {"data-set-floor":"1"})
+
+        dj_names = self.extract_djs(first_floor)
+
+class ArchiveDigging:
+
+    def __init__(self, ) -> None:
+        pass
+
+
+
 def get_dj_label(dj_container):
     try:
         return dj_container.find("span",  {"class": "font-normal text-sm md:text-md lowercase"}).text
@@ -22,6 +66,10 @@ def extract_djs(floor):
     dj_names = [get_dj_name(dj) for dj in djs]
 
     return dj_names
+
+def 
+
+def get_night_pages()
 
 
 if __name__ == "__main__":
