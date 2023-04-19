389026d23de915ebfa9f2b05e34396eb62626ca6
marc.than-van-con <marc.than-van-con@idealo.de>
2023-04-19 10:08:16 +0200

    Create class main and extracting one year of events

--- get_data/main.py
+++ get_data/main.py
@@ -1,31 +1,59 @@
 import json
+import itertools
 
 from event_page_parser import EventPageParser
 from month_page_parser import MonthPageParser
 
+
+class Main:
+
+    def __init__(self):
+
+        self.base_archive_url = "https://www.berghain.berlin/en/program/archive/"
+        self.base_event_url = "https://www.berghain.berlin"
+
+    def _format_month(self, month):
+
+        return str(month) if month > 9 else f"0{month}"
+
+    def fetch_month_events(self, year, month):
+        formated_month = self._format_month(month)
+        url = f"{self.base_archive_url}{year}/{formated_month}/"
+
+        events_extractor = MonthPageParser(url)
+        event_ids = events_extractor.extract()
+
+        events = []
+
+        for id in event_ids:
+            url = self.base_event_url + id
+            timetable_extractor = EventPageParser(url)
+
+            event = timetable_extractor.extract()
+            events.append(event)
+
+        return events
+
+    def fetch_years_events(self, year):
+        years_events = []
+        for i in range(1, 13):
+            try:
+                years_events.append(self.fetch_month_events(year, i))
+            except Exception as e:
+                continue
+
+        return list(itertools.chain.from_iterable(years_events))
+
+    def write_to_json(self, events):
+        json_object = json.dumps(events)
+
+        with open("sample.json", "w") as outfile:
+            outfile.write(json_object)
+
+
 if __name__ == "__main__":
-    # url = "https://www.berghain.berlin/en/event/73053/"
-    # timetable_extractor = TimeTableExtractor(url)
 
-    # dj_names = timetable_extractor.extract()
-    # print(dj_names)
+    main = Main()
+    years_events = main.fetch_years_events(2016)
 
-    base_path = "https://www.berghain.berlin"
-
-    url = "https://www.berghain.berlin/en/program/archive/2019/12/"
-    events_extractor = MonthPageParser(url)
-    links = events_extractor.extract()
-    events =[]
-    for link in links:
-        url = base_path + link
-        timetable_extractor = EventPageParser(url)
-
-        event = timetable_extractor.extract()
-        events.append(event)
-
-
-    json_object = json.dumps(events)
-
-    with open("sample.json", "w") as outfile:
-        outfile.write(json_object)
-
+    print(years_events)
