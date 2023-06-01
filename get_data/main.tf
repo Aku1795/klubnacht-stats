af3e5d70318b0febfd4fa268d5cab49097175a9d
marc.than-van-con <marc.than-van-con@idealo.de>
2023-06-01 20:27:43 +0200

    Adding scheduler

--- get_data/main.tf
+++ get_data/main.tf
@@ -26,7 +26,6 @@ resource "google_project_service" "scheduler_api" {
 
 
 ### Scrapper
-
 resource "google_artifact_registry_repository" "scraper_repo" {
   location      = var.location
   repository_id = "klubnacht-scraper"
@@ -67,4 +66,49 @@ resource "google_cloud_run_service" "scraper" {
     percent         = 100
     latest_revision = true
   }
+}
+
+### Scheduler
+resource "google_service_account" "scheduler" {
+  account_id   = "scheduler-sa"
+  description  = "Cloud Scheduler service account; used to trigger scheduled Cloud Run jobs."
+  display_name = "scheduler-sa"
+
+  # Use an explicit depends_on clause to wait until API is enabled
+  depends_on = [
+    google_project_service.iam_api
+  ]
+}
+
+resource "google_cloud_run_service_iam_member" "scheduler" {
+  location = google_cloud_run_service.scraper.location
+  service  = google_cloud_run_service.scraper.name
+  role     = "roles/run.invoker"
+  member   = "serviceAccount:${google_service_account.scheduler.email}"
+}
+
+resource "google_cloud_scheduler_job" "scrapper_scheduler" {
+  name             = "scraper_scheduler"
+  description      = "scraper_scheduler"
+  schedule         = "0 5 5 * *"
+  region           = var.location
+  attempt_deadline = "320s"
+
+  retry_config {
+    retry_count = 1
+  }
+
+  http_target {
+    http_method = "GET"
+    uri         = "${google_cloud_run_service.scraper.status[0].url}/scrap_last_month"
+    oidc_token {
+      service_account_email = google_service_account.scheduler.email
+    }
+  }
+
+  depends_on = [
+    google_project_service.scheduler_api
+  ]
+
+
 }
