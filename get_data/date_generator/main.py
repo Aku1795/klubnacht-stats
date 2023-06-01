0ef218150df069e3c47d86b0c797c33b67583956
marc.than-van-con <marc.than-van-con@idealo.de>
2023-06-01 13:24:41 +0200

    Modify dategenerator google function

--- get_data/date_generator/main.py
+++ get_data/date_generator/main.py
@@ -3,10 +3,21 @@ from datetime import date
 from flask import jsonify
 
 
+def compute_last_year_month(current_date):
+    if current_date.month == 1:
+        year = current_date.year - 1
+        month = 12
+    else:
+        year = current_date.year
+        month = current_date.month - 1
+
+    return year, month
+
 @functions_framework.http
 def current_date(request):
     current_date = date.today()
-    output = {"year": current_date.year, "month": current_date.month}
+    last_month_year, last_month = compute_last_year_month(current_date)
+    output = {"year": last_month_year, "month": last_month}
     return jsonify(output)
 
 
